6c41459709ac7d9537c639d0d8dd8707bfd05109
MUTHYALA VENGAL REDDY <muthyala.vengalreddy@hcl.com>
2021-12-12 16:09:32 +0530

    first

---
+++ Week10Assignment/src/main/java/com/test/demo/Week10AssignmentApplication.java
@@ -0,0 +1,14 @@
+package com.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week10AssignmentApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
